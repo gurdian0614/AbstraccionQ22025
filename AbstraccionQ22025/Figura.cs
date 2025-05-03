cf03844e89ac9ceeaed8236c750835489ab4ae08
gurdian0614 <ivan.gurdian@outlook.com>
2025-05-02 19:12:59 -0600

    Create abstract class

---
+++ AbstraccionQ22025/Figura.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace AbstraccionQ22025
+{
+    public abstract class Figura
+    {
+        public abstract double Area();
+        public abstract double Perimetro();
+        public void Imprimir(string NombreFigura)
+        {
+            Console.WriteLine($"Area del {NombreFigura}: {Area()} cm2");
+            Console.WriteLine($"Perímetro del {NombreFigura}: {Perimetro()} cm");
+        }
+    }
+}
