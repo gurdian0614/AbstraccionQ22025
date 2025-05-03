75fb246388c4b0d2a3145543ce11aa9e0ac0ffdb
gurdian0614 <ivan.gurdian@outlook.com>
2025-05-02 19:13:18 -0600

    Implement data

--- AbstraccionQ22025/Program.cs
+++ AbstraccionQ22025/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using AbstraccionQ22025;
+
+Circulo circulo = new Circulo();
+Rectangulo rectangulo = new Rectangulo();
+
+const string CIRCULO = "Círculo";
+const string RECTANGULO = "Rectángulo";
+
+Console.Write("Ingrese el Radio: ");
+circulo.Radio = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Ingrese la Base: ");
+rectangulo.Base = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Ingrese la Altura: ");
+rectangulo.Altura = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine();
+Console.WriteLine($"{CIRCULO}:");
+circulo.Imprimir(CIRCULO);
+
+Console.WriteLine();
+Console.WriteLine($"{RECTANGULO}:");
+rectangulo.Imprimir(RECTANGULO);
