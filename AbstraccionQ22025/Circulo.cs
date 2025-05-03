f2796f4abe8e7c99126fedfeb1a30b1f4f66f27e
gurdian0614 <ivan.gurdian@outlook.com>
2025-05-02 19:13:08 -0600

    Implement abstract class

---
+++ AbstraccionQ22025/Circulo.cs
@@ -0,0 +1,18 @@
+﻿
+namespace AbstraccionQ22025
+{
+    public class Circulo : Figura
+    {
+        public double Radio { get; set; }
+
+        public override double Area()
+        {
+            return Math.PI * Math.Pow(Radio, 2);
+        }
+
+        public override double Perimetro()
+        {
+            return 2 * Math.PI * Radio;
+        }
+    }
+}
---
+++ AbstraccionQ22025/Rectangulo.cs
@@ -0,0 +1,19 @@
+﻿
+namespace AbstraccionQ22025
+{
+    public class Rectangulo : Figura
+    {
+        public double Base { get; set; }
+        public double Altura { get; set; }
+
+        public override double Area()
+        {
+            return Math.Round(Base * Altura);
+        }
+
+        public override double Perimetro()
+        {
+            return Math.Round(2 * (Base + Altura));
+        }
+    }
+}
